dc8e05fd31f962c916d6bbd6c4346f90fa066507
atolbmaker <84431861+atolbmaker@users.noreply.github.com>
2021-06-09 13:35:29 -0600

    Delete Wrapper for C# algorithms directory

--- Wrapper for C# algorithms/CleanBinaries.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-@echo Attempting to clean binaries
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
-@echo Finished
-pause > nul
